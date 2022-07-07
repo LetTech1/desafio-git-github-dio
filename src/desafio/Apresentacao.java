f2be1c8d08ecc6bf3503cf225bae592e3fdeba45
Letícia Félix <leticiafelix2799@gmail.com>
2022-07-07 18:31:35 -0300

    Primeiro desafio Code Girls 2022

---
+++ src/desafio/Apresentacao.java
@@ -0,0 +1,10 @@
+package desafio;
+
+public class Apresentacao {
+	public static void main(String[] args) {
+		System.out.println("Oii pessoal, meu nome é Letícia, tenho 23 anos. Sou formada em Engenharia de Computação e estou muito feliz por ter conseguido"
+				+ "\npassar na primeira fase do Code Girls 2022. Com isso, no primeiro desafio do bootcamp, realizei o resumo do curso sobre Git/GitHub que tivemos."
+				+ "\nEspero que gostem e que os ajude de alguma forma! :)");
+	}
+
+}
